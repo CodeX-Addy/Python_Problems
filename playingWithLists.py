6920a273d1bd422f0d6e557d5cd97e0555a91f35
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-29 12:58:09 +0530

    Create playingWithLists.py

---
+++ playingWithLists.py
@@ -0,0 +1,14 @@
+list1 = [2,6,8,9,10]
+#Traversing
+print(list1)
+#Traversing upto some index
+print(list1[0:4])#Prints upto 3rd index
+
+print(list1[-4:-1]) 
+
+list2 = [1,5,7]
+print(list1+list2) #it concatinates the two lists(Union without sorting) 
+
+l1 = [2,4,6]
+l2 = [1,2,5]
+print(l1+l2+list1+list2) #Can be simulatenously add more than two lists
