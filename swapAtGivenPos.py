a4f1ae92be47e3a9156c3d03eae735aac695992b
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-18 01:14:42 +0530

    Create swapAtGivenPos.py

---
+++ swapAtGivenPos.py
@@ -0,0 +1,12 @@
+def swapPositions(list, pos1, pos2):
+	
+	list[pos1], list[pos2] = list[pos2], list[pos1]
+	return list
+
+# Driver function
+List = [23, 65, 19, 90]
+pos1, pos2 = 1, 3
+
+print(swapPositions(List, pos1-1, pos2-1))
+
+# Output will be [19, 65, 23, 90]. Since the indexing in lists starts from 1 !!
