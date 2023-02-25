9cf9ab21407776f728be594467c88a5ef104a0cc
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-25 19:03:57 +0530

    Create maxAndMinNo.py

---
+++ maxAndMinNo.py
@@ -0,0 +1,18 @@
+def MAX(sets):
+	return (max(sets))
+	
+# Driver Code
+sets = set([8, 16, 24, 1, 25, 3, 10, 65, 55])
+print(MAX(sets)) 
+# Output will be 65
+
+def MIN(sets):
+	return (min(sets))
+	
+# Driver Code
+sets = set([4, 12, 10, 9, 4, 13])
+print(MIN(sets))
+# Output will be 4
+
+
+
