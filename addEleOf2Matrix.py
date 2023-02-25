439919075f3e59f79583ecf5a2ed970d294f8a0d
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-25 19:10:22 +0530

    Create addEleOf2Matrix.py

---
+++ addEleOf2Matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+A = np.array([[1, 2], [3, 4]])#First matrix
+
+B = np.array([[4, 5], [6, 7]]) #Second matrix
+print(A)
+print(B)
+
+print("Addition of two matrix")
+print(np.add(A, B))#Adding the elements
