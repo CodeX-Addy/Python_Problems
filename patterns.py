496ff571c69ec0bd5ce611b6f21a80ab661052cd
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-04-06 19:12:26 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -49,4 +49,28 @@ Diamond(rows)
   * * * 
    * * 
     * 
+# Digit pattern
+# function to print the pattern
+def pattern(n):
 
+	# traverse through the elements
+	# in n assuming it as a string
+	for i in n:
+
+		# print | for every line
+		print("|", end = "")
+
+		# print i number of * s in
+		# each line
+		print("*" * int(i))
+
+# get the input as string		
+n = "41325"
+pattern(n)
+
+# Output:-
+# |****
+# |*
+# |***
+# |**
+# |*****
