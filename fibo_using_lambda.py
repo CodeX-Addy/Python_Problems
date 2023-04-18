78fc9af71c75ccb63770772ff73cd97822469d6e
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-04-18 20:12:34 +0530

    Update fibo_using_lambda.py

--- fibo_using_lambda.py
+++ fibo_using_lambda.py
@@ -3,3 +3,15 @@ from functools import reduce
 
 fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]],range(n-2), [0, 1]
 print(fib(5))
+                       
+# By using lambda and map function
+def fibonacci(count):
+	fib_list = [0, 1]
+
+	any(map(lambda _: fib_list.append(sum(fib_list[-2:])),
+										range(2, count)))
+
+	return fib_list[:count]
+
+print(fibonacci(10))
+
