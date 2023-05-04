7b470b7d964839033aa80794b92b56ad9ad09232
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-04 20:44:08 +0530

    Update prettyPrint.py

--- prettyPrint.py
+++ prettyPrint.py
@@ -10,22 +10,19 @@ class LinkedList:
 
 	def __str__(self):
 	
-		# defining a blank res variable
+		# Defining a blank res variable
 		res = ""
 		
-		# initializing ptr to head
+		# Initializing ptr to head
 		ptr = self.head
 		
-	# traversing and adding it to res
 		while ptr:
 			res += str(ptr.val) + ", "
 			ptr = ptr.next
 
-	# removing trailing commas
+	# Removing trailing commas
 		res = res.strip(", ")
 		
-		# chen checking if
-		# anything is present in res or not
 		if len(res):
 			return "[" + res + "]"
 		else:
@@ -34,7 +31,7 @@ class LinkedList:
 
 if __name__ == "__main__":
 
-	# defining linked list
+	# Defining a linked list
 	ll = LinkedList()
 
 	# defining nodes
@@ -42,11 +39,8 @@ if __name__ == "__main__":
 	node2 = Node(15)
 	node3 = Node(20)
 
-	# connecting the nodes
+	# Connecting the nodes
 	ll.head = node1
 	node1.next = node2
 	node2.next = node3
-	
-	# when print is called, by default
-	#it calls the __str__ method
 	print(ll)
