e2195a7eaa3f2e27f2b37839b89c1826828342f9
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-08 15:09:28 +0530

    Update nQueen.py

--- nQueen.py
+++ nQueen.py
@@ -29,26 +29,18 @@ def isSafe(board, row, col):
 	return True
 
 def solveNQUtil(board, col):
-	# base case: If all queens are placed
-	# then return true
 	if col >= N:
 		return True
-
-	# Consider this column and try placing
-	# this queen in all rows one by one
 	for i in range(N):
 
 		if isSafe(board, i, col):
 			# Place this queen in board[i][col]
 			board[i][col] = 1
 
-			# recur to place rest of the queens
 			if solveNQUtil(board, col + 1) == True:
 				return True
 
 			# If placing queen in board[i][col
-			# doesn't lead to a solution, then
-			# queen from board[i][col]
 			board[i][col] = 0
 
 	return False
