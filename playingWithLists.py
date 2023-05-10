45a0515eba1baaa04461f1281c5d92dacb80fce6
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-10 21:50:27 +0530

    Update playingWithLists.py

--- playingWithLists.py
+++ playingWithLists.py
@@ -12,3 +12,11 @@ print(list1+list2) #it concatinates the two lists(Union without sorting)
 l1 = [2,4,6]
 l2 = [1,2,5]
 print(l1+l2+list1+list2) #Can be simulatenously add more than two lists
+
+#Append function
+list1 = [2,3,4]
+list1.append(2)
+print(list1)
+#Output:-[2, 3, 4, 2]
+
+
