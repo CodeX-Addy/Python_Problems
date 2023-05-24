079d3b0051f88afa91a8f0b409ec66846a3d280a
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-24 18:09:13 +0530

    Update recursions.py

--- recursions.py
+++ recursions.py
@@ -7,3 +7,13 @@ def power(a, b):
         return a*power(a,b-1)
 
 print(power(2,5))
+
+#Factorial of a number 
+def factorial(x):
+    if (x==0 or x==1):
+        return 1
+    else:
+        return x*factorial(x-1)
+        
+a = int(input("Enter your number:"))
+print(factorial(a))
