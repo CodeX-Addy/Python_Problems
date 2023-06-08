2aad8340b9f3aec0c24629ea3dfc3bda80a0b4c2
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-06-08 16:58:32 +0530

    Update itertools.py

--- itertools.py
+++ itertools.py
@@ -1,2 +1,8 @@
+#itertools.product()
 from itertools import product 
 print(*product(list(map(int,input().split())),list(map(int,input().split()))))
+
+#itertools.permutations()
+from itertools import permutations
+S, k = input().split()
+print('\n'.join(sorted(map(''.join,permutations(S,int(k))))))
