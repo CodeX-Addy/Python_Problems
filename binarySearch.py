3b2fd00c8acc5f4f3e6196376f55b1438ba0665a
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-07-31 11:32:32 +0530

    Create binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,21 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+def binarySearch(arr,s,e,k):
+    if s<=e:
+        mid = (s+e)//2
+        if arr[mid] < k:
+            return binarySearch(arr, mid+1, e, k)
+        elif arr[mid] > k:
+            return binarySearch(arr, s, mid-1, k)
+        else:
+            return mid
+    else:
+        return -1
+
+li = list(map(int, input().split()))
+pos = binarySearch(li, 0, len(li), 1)
+if pos!=1 :
+    print("Element is present at " + str(pos))
+else:
+    print("Not present")
+            
